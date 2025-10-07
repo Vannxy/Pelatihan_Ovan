84ab8f92fa5e3ddd98025c48ae6f8ba55db78895
VannSky <ovan.a.2563@gmail.com>
2025-10-07 13:15:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const tombolSapa = document.getElementById('sapaButton');
+document.addEventListener('DOMContentLoaded', function() {
+  const tombolSapa = document.getElementById('sapaButton');
   tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih telah menyapa saya!');
 });
